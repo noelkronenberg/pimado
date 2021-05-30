d541d1feef252fefeed8491d010ff2942b9b30ae
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2021-05-30 14:21:13 +0200

    added variable to do items to the front end
    variable to do items are now visible in the front end

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoItem
 
 def todoView(request):
-    return render(request, 'todo.html')
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html', 
+    {'all_items': all_todo_items})
