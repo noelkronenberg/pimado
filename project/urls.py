38c95eea65570b09add5fc26aee9560d37ea7a1b
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2021-05-31 17:42:43 +0200

    added favicon

--- project/urls.py
+++ project/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from todo.views import addTodo, todoView, deleteTodo, dashboardView, download_file
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
